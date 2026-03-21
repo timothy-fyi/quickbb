c220248247e70ee60e718d59e497706ec9321aff
Tim <tjflanagan27@gmail.com>
2026-03-21 16:44:16 -0400

    code refactor, quickbb is now reusable

--- quickbb.py
+++ quickbb.py
@@ -3,64 +3,72 @@ import json
 import os
 import subprocess
 import sys
-import time
 import requests
 import yaml
 from PIL import Image
+from PIL import UnidentifiedImageError
 
-api_key_file = os.path.join(os.path.dirname(__file__), 'api_key.yaml')
 
-try:
-    with open(api_key_file, 'r') as api_key_load:
-        api_key_var = yaml.safe_load(api_key_load)
-except FileNotFoundError:
-    with open(api_key_file, 'w+') as api_key_load:
-        api_key_load.write(f'key: ')
-    print(
-        f'"api_key.yaml" does not exist and has been created. '
-        'Please edit the newly created "api_key.yaml" file, add your ImgBB API key, and re-run quickbb.'
-    )
-    exit()
+def bb_upload(api_key: str, image_file: str, copy_to_clipboard: bool = False) -> str:
+    try:
+        Image.open(image_file)
+    except UnidentifiedImageError:
+        raise Exception("This is not an image file, please try again.")
+    except FileNotFoundError:
+        raise Exception(
+            "File not found, please check the file path/file name and try again."
+        )
 
-api_key = api_key_var['key']
+    with open(image_file, "rb") as file:
+        url = "https://api.imgbb.com/1/upload"
+        payload = {
+            "key": api_key,
+            "image": base64.b64encode(file.read()),
+        }
+        res = requests.post(url, payload)
 
-# if context menu is used
-try:
-    image_file = sys.argv[1]
-# drag and drop/manual input method if context menu isn't used
-except IndexError:
-    image_file = input("Enter image location: ")
+    if res.status_code == 400:
+        raise Exception("Error communicating with ImgBB. Check your API key.")
 
-# validate file is an image file
-try:
-    Image.open(image_file)
-except OSError:
-    print("This is not an image file, please close this window and try again.")
-    time.sleep(7)
-    sys.exit(1)
+    bb_data = res.text
+    bb_parsed = json.loads(bb_data)
 
-print('Uploading to ImgBB...')
+    img_url = bb_parsed["data"]["image"]["url"]
 
-with open(image_file, 'rb') as file:
-    url = 'https://api.imgbb.com/1/upload'
-    payload = {
-        'key': api_key,
-        'image': base64.b64encode(file.read()),
-    }
-    res = requests.post(url, payload)
+    if copy_to_clipboard:
+        cmd = "echo " + img_url.strip() + "|clip"
+        subprocess.check_call(cmd, shell=True)
 
-if res.status_code == 400:
-    print('Error communicating with ImgBB. Check your API key.')
-    time.sleep(7)
-    sys.exit(1)
+    return img_url
 
-bb_data = res.text
-bb_parsed = json.loads(bb_data)
 
-img_url = bb_parsed['data']['image']['url']
+def main():
+    api_key_file = os.path.join(os.path.dirname(__file__), "api_key.yaml")
 
-# copies to clipboard
-cmd='echo '+img_url.strip()+'|clip'
-subprocess.check_call(cmd, shell=True)
+    try:
+        with open(api_key_file, "r") as api_key_load:
+            api_key_var = yaml.safe_load(api_key_load)
+    except FileNotFoundError:
+        with open(api_key_file, "w+") as api_key_load:
+            api_key_load.write(f"key: ")
+        print(
+            f'"api_key.yaml" does not exist and has been created. '
+            'Please edit the newly created "api_key.yaml" file, add your ImgBB API key, and re-run quickbb.'
+        )
+        exit()
 
-print('Done!')
+    api_key = api_key_var["key"]
+
+    # if context menu/terminal is used
+    try:
+        image_file = sys.argv[1]
+    # drag and drop/manual input method if context menu isn't used
+    except IndexError:
+        image_file = input("Enter image location: ")
+
+    bb_upload(api_key=api_key, image_file=image_file, copy_to_clipboard=True)
+
+
+if __name__ == "__main__":
+    main()
+ 
--- tools/imgbb.cmd
+++ tools/imgbb.cmd
@@ -1,4 +1,6 @@
 @echo off
 cls
+echo Uploading image to ImgBB...
 :: update path to script after 'python' and before '%1'
 python C:\path\to\quickbb\quickbb.py %1
+echo Complete!
